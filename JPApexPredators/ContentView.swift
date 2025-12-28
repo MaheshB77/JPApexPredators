33a7e0fa000573e84ac438b48b247bc2ffd2071d
MaheshB77 <mbansode7696@gmail.com>
2025-12-28 23:58:35 +0530

    Added navigation and search

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -10,29 +10,53 @@ import SwiftUI
 struct ContentView: View {
     let predatorService = PredatorService()
 
+    @State var searchStr = ""
+
+    var filteredPredators: [ApexPredator] {
+        if searchStr.isEmpty {
+            return predatorService.apexPredators
+        } else {
+            return predatorService.apexPredators.filter {
+                predator in
+                predator.name.lowercased().contains(searchStr.lowercased())
+            }
+        }
+    }
+
     var body: some View {
-        List(predatorService.apexPredators) { predator in
-            
-            HStack {
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading) {
-                    Text(predator.name)
-                        .font(.title3)
-                    
-                    // Predator type chip
-                    Text(predator.type.rawValue.capitalized)
-                        .padding(.horizontal, 8)
-                        .padding(.vertical, 4)
-                        .background(predator.type.background)
-                        
-                        .clipShape(.capsule)
-                        
+        NavigationStack {
+            List(filteredPredators) { predator in
+
+                NavigationLink {
+
+                } label: {
+                    HStack {
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+
+                        VStack(alignment: .leading) {
+                            Text(predator.name)
+                                .font(.title3)
+
+                            // Predator type chip
+                            Text(predator.type.rawValue.capitalized)
+                                .padding(.horizontal, 8)
+                                .padding(.vertical, 4)
+                                .background(predator.type.background)
+
+                                .clipShape(.capsule)
+
+                        }
+                    }
                 }
+                .navigationTitle("Apex Predators")
+                .searchable(text: $searchStr, prompt: "Search Apex Predators")
+                .autocorrectionDisabled()
+                .animation(.default, value: searchStr)
+
             }
         }.preferredColorScheme(.dark)
     }
