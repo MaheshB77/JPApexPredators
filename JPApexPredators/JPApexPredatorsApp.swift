aaf1002ab8e72ce1851734d688f9fd5cdefd1d51
MaheshB77 <mbansode7696@gmail.com>
2025-12-28 17:59:11 +0530

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Mahesh Bansode on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
